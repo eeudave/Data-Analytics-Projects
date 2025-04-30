8f616ecc071b88f2fdcf73ef7b93d741049fd2f9
eeudave <erik.eudave@gmail.com>
2025-04-30 18:07:26 +0200

    Cheese

--- Cheese/app.py
+++ Cheese/app.py
@@ -180,7 +180,7 @@ with st.container():
                 legend_title="Tipo de leche",
                 paper_bgcolor="#082e6e",   # Fondo de la figura
                 plot_bgcolor="#082e6e",    # Fondo del área del gráfico
-                font=dict(color="#fc8c1c") # Color del texto
+                font=dict(color="#fc8c1c"), # Color del texto
                 legend=dict(
                     font=dict(color="#f5eec6"),
                     title_font=dict(color="#fc8c1c")
